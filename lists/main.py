b31f8eff12a7feb3ace3d2ad05cc56221bfc2490
human2402 <65188741+human2402@users.noreply.github.com>
2025-12-02 01:56:05 +0900

    дорботка задания на списки

--- lists/main.py
+++ lists/main.py
@@ -1,17 +1,38 @@
+'''
+    Выполнил Малежик Дмитприй
+    ИВТм-25
+
+    Массивы list и numpy array
+'''
+
 import numpy as np
 import timeit
-from utils import generate_list, generate_set, generate_numpy, standardize_and_sum_debug
+from utils import generate_list, generate_set, generate_numpy, standardize_and_sum
+from tests import test_standardize_small, test_standardize_with_zero, test_large_array
 
-# print (standardize_and_sum_debug(np.array([1, 2, 3])))
 
 
 def benchmark():
-    a, b, n = 1, 10**7, 10**4
-    print (generate_list(n, a, b))
+    a, b, n = 1, 10**7, 10**8
+    # print (generate_list(n, a, b))
 
     print("List:", timeit.timeit(lambda: generate_list(n, a, b), number=3))
     print("Set:", timeit.timeit(lambda: generate_set(n, a, b), number=3))
     print("Numpy:", timeit.timeit(lambda: generate_numpy(n, a, b), number=3))
 
+def calculate():
+    a, b, n = 1, 1000, 10
+    new_numpy = generate_numpy(n, a, b)
+    print ("Массив -", new_numpy)
+    print ("Сумма элементов стандартизированного массива -",
+           standardize_and_sum(new_numpy))
+    
+def test():
+    test_standardize_small()
+    test_standardize_with_zero()
+    test_large_array()
+
 if __name__ == "__main__":
-    benchmark()
+    # benchmark()
+    calculate()
+    test()
